1cb2e904c681d02174524c5e497e263e6cf6462a
Xum <xum2813@gmail.com>
2021-05-08 15:53:30 +0800

    Create subapp_v1.sh

---
+++ Linux/subapp_v1.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# 最好先安装zsh并修改插件选项
+# 如果家目录没有app文件夹
+cd ~ && makdir app
+cd ~/app
+
+# 安装htop
+wget -qO- wget http://hisham.hm/htop/releases/2.2.0/htop-2.2.0.tar.gz | tar xvz -C .
+mkdir htop
+cd htop-2.2.0/
+./configure --prefix=$HOME/app/htop
+make && make install
+cd ~/app
+rm -rf htop-2.2.0
+
+
+# 安装axel
+wget -c https://github.91chifun.workers.dev/https://github.com//axel-download-accelerator/axel/releases/download/v2.17.10/axel-2.17.10.tar.gz
+x axel-2.17.10.tar.gz
+mkdir axel
+cd axel-2.17.10
+./configure --prefix=$HOME/app/axel
+make && make install
+cd ~/app
+rm -rf axel-2.17.10
+
+# 安装ag
+git clone https://hub.fastgit.org/ggreer/the_silver_searcher.git
+mkdir ag
+cd the_silver_searcher
+./build.sh
+./configure --prefix=$HOME/app/ag
+make && make install
+
+# 安装duf
+wget -c https://github.91chifun.workers.dev/https://github.com//muesli/duf/releases/download/v0.6.2/duf_0.6.2_linux_x86_64.tar.gz
+x duf_0.6.2_linux_x86_64.tar.gz
+mv duf_0.6.2_linux_x86_64 duf
+
+# bat
+
+
+
+
+
+
+
